758ca6a40f3095aeb2600d8f6cab0180ca8b5eae
RM-71 <Reymark_decastro@dlsl.edu.ph>
2025-08-16 07:39:48 +0800

    ProjectData.js

--- src/Image/ProjectData.js
+++ src/Image/ProjectData.js
@@ -58,17 +58,17 @@ export const projects = [
     screens: [
       {
         img: Home,
-        title: 'Agriculture IoT Greenhouse Automation', // Modal caption
+        title: 'Smart Home Energy Management', // Modal caption
         description: 'system that uses IoT devices to monitor, control, and optimize energy usage in a home. It helps track real-time power consumption, automate appliances, and reduce energy waste, leading to increased efficiency and lower electricity bills.'
       },
       {
         img: Agriculture,
-        title: 'CCTV Camera Surveillance System', // Modal caption
+        title: 'Agriculture IoT Greenhouse Automation', // Modal caption
         description: 'a smart farming project that uses IoT sensors and devices to monitor and control environmental conditions inside a greenhouse. It automates irrigation, temperature, humidity, and light control to optimize plant growth, reduce manual labor, and increase crop yield efficiently.'
       },
       {
         img: Camera,
-        title: 'Smart Home Energy Management', // Modal caption
+        title: 'CCTV Camera Surveillance System', // Modal caption
         description: 'a smart surveillance system that combines IoT connectivity with deep learning algorithms to detect, recognize, and alert suspicious activities in real-time. It enables intelligent video analysis, such as facial recognition and motion detection, enhancing security and reducing false alarms.'
       }
     ]
